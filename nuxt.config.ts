686ba4d83f44c85e908192f7169c8156c6247c45
cquijad2 <cquijad2@externos.bancoestado.cl>
2026-02-27 12:19:59 -0300

    actualizacion workbox

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,42 @@
+// export default defineNuxtConfig({
+//   ssr: false,
+//   modules: ['@vite-pwa/nuxt', '@nuxt/ui'],
+
+//   app: {
+//     head: {
+//       meta: [{ name: 'theme-color', content: '#0b5fff' }],
+//       link: [
+//         { rel: 'icon', type: 'image/png', href: '/pwa-192x192.png' },
+//         { rel: 'apple-touch-icon', href: '/pwa-192x192.png' }
+//       ]
+//     }
+//   },
+
+//   pwa: {
+//     registerType: 'autoUpdate',
+//     manifest: {
+//       name: 'POC Afiliación Offline',
+//       short_name: 'Afiliación',
+//       start_url: '/',
+//       scope: '/',
+//       display: 'standalone',
+//       background_color: '#ffffff',
+//       theme_color: '#0b5fff',
+//       icons: [
+//         { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+//         { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+//       ]
+//     },
+//     devOptions: {
+//       enabled: true
+//     }
+//   },
+
+//   runtimeConfig: {
+//     public: { apiBase: '' }
+//   }
+// })
+
 export default defineNuxtConfig({
   ssr: false,
   modules: ['@vite-pwa/nuxt', '@nuxt/ui'],
@@ -14,6 +53,8 @@ export default defineNuxtConfig({
 
   pwa: {
     registerType: 'autoUpdate',
+    strategies: 'generateSW',
+
     manifest: {
       name: 'POC Afiliación Offline',
       short_name: 'Afiliación',
@@ -27,8 +68,26 @@ export default defineNuxtConfig({
         { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
       ]
     },
+
+    workbox: {
+      navigateFallback: '/',
+      navigateFallbackDenylist: [/^\/api\//],
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest,woff2}'],
+
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.mode === 'navigate',
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'pages',
+            networkTimeoutSeconds: 3
+          }
+        }
+      ]
+    },
+
     devOptions: {
-      enabled: true
+      enabled: false
     }
   },
 
