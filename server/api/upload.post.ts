4aba55bdc5db0452450941c67597ba025aca2bee
cquijad2 <cquijad2@externos.bancoestado.cl>
2026-03-02 12:05:09 -0300

    actualizacion path backoffice

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
-  const target = 'https://pwa-nuxt-fuerza-upload.vercel.app' //`${config.public.apiBase}/tracking/sitpriv/upload`; //'http://localhost:3001/tracking/sitpriv/upload'
+  const target = 'https://v9k9214s-3001.brs.devtunnels.ms' //`${config.public.apiBase}/tracking/sitpriv/upload`; //'http://localhost:3001/tracking/sitpriv/upload'
   
 
   const ct = getHeader(event, 'content-type') || ''
